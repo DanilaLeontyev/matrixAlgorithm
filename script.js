b0024dc7dce2ddc2760804383b09549e591581a2
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-08-04 21:06:32 +0500

    добавил подсчет доменов

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let areaNumber = 2;
 const createMatrix = document.querySelector('#createMatrix')
 const calcDomain = document.querySelector('#calcDomain')
 const autoMatrix = document.querySelector('#autoMatrix')
+const matrixContainer = document.querySelector('.matrix-container')
+const domainCountOutput = document.querySelector('.domainCount')
 
 createMatrix.addEventListener('click', function () {
   const x = document.querySelector('#x').value
   const y = document.querySelector('#y').value
-  let matrixContainer = document.querySelector('.matrix-container')
   matrixContainer.appendChild(generateMatrix(x, y));
 })
 
 calcDomain.addEventListener('click', function () {
   clusterDomain(readMatrix())
+  getDomainCount()
 })
 
 autoMatrix.addEventListener('click', function () {
   const x = document.querySelector('#x').value
   const y = document.querySelector('#y').value
   const probability = document.querySelector('#probability').value
-  let matrixContainer = document.querySelector('.matrix-container')
   matrixContainer.appendChild(generateMatrix(x, y, probability));
 })
 
@@ -41,6 +41,10 @@ function generateMatrix(x, y, probability) {
   let colCount = y;
   let matrix = document.createElement('table')
   matrix.classList.add('matrix')
+
+  if (matrixContainer.hasChildNodes()) {
+    matrixContainer.removeChild(matrixContainer.childNodes[0])
+  }
 
   for (rowCount; rowCount > 0; rowCount--) {
     row = document.createElement('tr')
@@ -66,18 +70,37 @@ function generateMatrix(x, y, probability) {
 function changeValue() {
   if (this.textContent === '0') {
     this.textContent = 1
+    this.dataset.domain = 1
   }
-  else this.textContent = 0
+  else {
+    this.textContent = 0
+    this.dataset.domain = 0
+  }
+}
+
+function getDomainCount() {
+  let matrix = readMatrix()
+  let domainCount = 0;
+  matrix.forEach(function (row) {
+    row.forEach(function (elem) {
+      if (elem.dataset.domain > domainCount) {
+        domainCount = +elem.dataset.domain
+      }
+    })
+  })
+  domainCountOutput.innerHTML = `Всего доменов: ${domainCount}`
 }
 
 function clusterDomain(matrix) {
+  let areaNumber = 2;
+  domainCount = 0;
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j].dataset.domain === '1') {
         let color = getRandomColor();
         matrix[i][j].dataset.domain = areaNumber;
         matrix[i][j].style.backgroundColor = color;
-        checkNeighbors(i, j, matrix, color);
+        checkNeighbors(i, j, matrix, color, areaNumber);
         areaNumber++;
       }
     }
@@ -86,16 +109,7 @@ function clusterDomain(matrix) {
   return matrix;
 }
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function checkNeighbors(i, j, matrix, color) {
+function checkNeighbors(i, j, matrix, color, areaNumber) {
   if (
     j + 1 < matrix[i].length &&
     matrix[i][j + 1].dataset.domain === '1' &&
@@ -103,13 +117,13 @@ function checkNeighbors(i, j, matrix, color) {
   ) {
     matrix[i][j + 1].dataset.domain = areaNumber;
     matrix[i][j + 1].style.backgroundColor = color;
-    checkNeighbors(i, j + 1, matrix, color);
+    checkNeighbors(i, j + 1, matrix, color, areaNumber);
   }
 
   if (j - 1 >= 0 && matrix[i][j - 1].dataset.domain === '1' && matrix[i][j - 1].dataset.domain !== areaNumber) {
     matrix[i][j - 1].dataset.domain = areaNumber;
     matrix[i][j - 1].style.backgroundColor = color;
-    checkNeighbors(i, j - 1, matrix, color);
+    checkNeighbors(i, j - 1, matrix, color, areaNumber);
   }
 
   if (
@@ -119,12 +133,21 @@ function checkNeighbors(i, j, matrix, color) {
   ) {
     matrix[i + 1][j].dataset.domain = areaNumber;
     matrix[i + 1][j].style.backgroundColor = color;
-    checkNeighbors(i + 1, j, matrix, color);
+    checkNeighbors(i + 1, j, matrix, color, areaNumber);
   }
 
   if (i - 1 >= 0 && matrix[i - 1][j].dataset.domain === '1' && matrix[i - 1][j].dataset.domain !== areaNumber) {
     matrix[i - 1][j].dataset.domain = areaNumber;
     matrix[i - 1][j].style.backgroundColor = color;
-    checkNeighbors(i - 1, j, matrix, color);
+    checkNeighbors(i - 1, j, matrix, color, areaNumber);
   }
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
